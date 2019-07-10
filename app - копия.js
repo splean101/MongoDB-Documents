4d0da72afec363b11d9d1c26e690b7abeb5a2c1b
splean101 <101splean@gmail.com>
2019-07-10 15:34:25 +0300

    updateOne

--- app - копия.js
+++ app - копия.js
@@ -17,17 +17,7 @@ const games = [
     new Game('Doom', 'Playstation 4', 'id Software', 'First Person Shooter', 20)
 ];
 
-(async function(){
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-    const db = client.db('gamesdb');
-    const coll = await db.collection('games');
-    //const res = await coll.insertMany(games);
-    //console.log(res);
-    const updOne = await coll.updateOne({name: 'Shatter'}, {$set: {price: 5}});
-    console.log(updOne.result);
-    client.close();    
-}());
-/*MongoClient.connect(
+MongoClient.connect(
     'mongodb://localhost:27017',
     { useNewUrlParser: true },
     (err, client) => {
@@ -40,4 +30,4 @@ const games = [
 
             });
         });
-    });*/
+    });
